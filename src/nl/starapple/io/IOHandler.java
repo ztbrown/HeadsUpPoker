8a0bd516df37965a37b436e4d5f2f1f2c75a0d9c
boreus <boreus@gmail.com>
2012-10-16 10:19:57 +0200

    Classes for running processes and communicating with them

---
+++ src/nl/starapple/io/IOHandler.java
@@ -0,0 +1,75 @@
+package nl.starapple.io;
+
+import java.io.IOException;
+
+public class IOHandler {
+
+	Process child;
+	InStream out, err;
+	OutStream in;
+	
+	public IOHandler(String command) throws IOException {
+		child = Runtime.getRuntime().exec(command);
+		in = new OutStream(child.getOutputStream());
+		out = new InStream(child.getInputStream());
+		err = new InStream(child.getErrorStream());
+		out.start(); err.start();
+	}
+	
+	public void stop() {
+		try {
+			in.close();
+			Thread.sleep(100);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			//e1.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+		}
+		out.finish();
+		err.finish();
+		child.destroy();
+		try {
+			child.waitFor();
+			out.join();
+			err.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String readLine(long timeout) {
+		//System.err.printf("readLine(%d)\n", timeout);
+		try {
+			in.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+		}
+		return out.readLine(timeout);
+	}
+	
+	public boolean writeLine(String line) {
+		//System.err.printf("writeLine(\"%s\")\n", line);
+		try {
+			in.writeLine(line);
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public String getStdin() {
+		return in.getData();
+	}
+	
+	public String getStdout() {
+		return out.getData();
+	}
+	
+	public String getStderr() {
+		return err.getData();
+	}
+}
---
+++ src/nl/starapple/io/IORobot.java
@@ -0,0 +1,52 @@
+package nl.starapple.io;
+
+import java.io.IOException;
+
+import nl.starapple.poker.MatchInfo;
+import nl.starapple.poker.PokerMove;
+import nl.starapple.poker.Robot;
+
+public class IORobot implements Robot {
+
+	IOHandler handler;
+
+	public IORobot(String command) throws IOException {
+		handler = new IOHandler(command);
+	}
+
+    @Override
+	public void writeMove(PokerMove move) {
+        handler.writeLine(move.toString());
+    }
+	
+    @Override
+	public PokerMove getMove(long timeOut) {
+		handler.writeLine("go "+timeOut);
+        String line = handler.readLine(timeOut);
+        if( line == null ) { return null; }
+        String[] parts = line.split("\\s");
+        assert( parts.length == 2 ) : String.format("Bot input ``%s'' does not split into two parts", line);
+        return new PokerMove(parts[0], Integer.valueOf(parts[1]));
+    }
+
+    @Override
+	public void writeInfo(MatchInfo info) {
+        handler.writeLine(info.toString());
+    }
+	
+	public void finish() {
+		handler.stop();
+	}
+
+	public String getStdin() {
+		return handler.getStdin();
+	}
+
+	public String getStdout() {
+		return handler.getStdout();
+	}
+
+	public String getStderr() {
+		return handler.getStderr();
+	}
+}
